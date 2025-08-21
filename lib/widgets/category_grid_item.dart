64f235319a53e049eea7e79589ba9571d7a6d0ea
himanshu <kh9027505673@gmail.com>
2025-08-21 15:26:42 +0530

    fix color

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -2,14 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key,required this .category});
+  const CategoryGridItem({super.key, required this.category});
 
   final Category category;
   @override
   Widget build(BuildContext context) {
-   return Container(
-    decoration: BoxDecoration(
-      gradient: LinearGradient(colors: colors)
-    ), );
+    return Container(
+      decoration: BoxDecoration(gradient: LinearGradient(colors: Colors.amber)),
+    );
   }
 }
